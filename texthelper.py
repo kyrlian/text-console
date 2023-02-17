54955b1adc67a3e42f0bf40edf348db0e20d3e12
serra <remi.serra@fr.ibm.com>
2023-02-17 17:58:25 +0100

    lint

--- texthelper.py
+++ texthelper.py
@@ -2,34 +2,34 @@
 
 class TextHelper:
 
-    def getline(ar, y):
+    def getline(self,ar, y):
         if (y < len(ar)):
             return ar[y]
         return ""
 
-    def getchar(ar, x, y):
+    def getchar(self,ar, x, y):
         if (y < len(ar)):
             if (x < len(ar[y])):
                 return ar[y][x:x+1]
         return ""
 
-    def mergetwoarrays(a, b):
+    def mergetwoarrays(self,a, b):
         o = []
         for y in range(max(len(a), len(b))):
             to = ""
-            la = TextHelper.getline(a, y)
-            lb = TextHelper.getline(b, y)
+            la = self.getline(a, y)
+            lb = self.getline(b, y)
             for x in range(max(len(la), len(lb))):
-                tto = TextHelper.getchar(a, x, y)
-                cb = TextHelper.getchar(b, x, y)
+                tto = self.getchar(a, x, y)
+                cb = self.getchar(b, x, y)
                 if cb != "" and cb != " ":
                     tto = cb
                 to += tto
             o.append(to)
         return o
 
-    def mergearrays(aa):
+    def mergearrays(self, aa):
         o = aa[0]
         for i in range(1, len(aa), 1):
-            o = TextHelper.mergetwoarrays(o, aa[i])
+            o = self.mergetwoarrays(o, aa[i])
         return o
