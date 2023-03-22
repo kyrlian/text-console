8807f3442a6e245e2824ce54338316e31b50dde4
serra <remi.serra@fr.ibm.com>
2023-03-22 18:07:31 +0100

    comments

--- panelsearch.py
+++ panelsearch.py
@@ -1,8 +1,5 @@
 # TODO search and display a simple result
+# use wikipedia API 
 
-#option1
-# use google or wikipedia API 
-
-#option2
-# use selenium to do a search on google or wikipedia and return 
-# https://www.selenium.dev/selenium/docs/api/py/
+import pygame
+import requests
